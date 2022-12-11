204af73cf141548b5bb40f40698495113dee566d
lz2510 <lizheng2510@gmail.com>
2022-12-11 12:37:25 +0800

    Update Solution.php

--- week_4/70_climbing-stairs/Solution.php
+++ week_4/70_climbing-stairs/Solution.php
@@ -20,19 +20,4 @@ class Solution {
         }
         return $dp[$n-1];
     }
-
-	/**
-	 * @param Integer $n
-	 * @return Integer
-	 */
-	function climbStairs210403($n) {
-		if($n == 1) return 1;
-		$dp = [];
-		$dp[1] = 1;
-		$dp[2] = 2;
-		for($i=3; $i<=$n; $i++){
-			$dp[$i] = $dp[$i-1] + $dp[$i-2];
-		}
-		return $dp[$n];
-	}
 }
